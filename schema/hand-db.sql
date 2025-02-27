5d79070018e782f0ea846184033fd3ef516ea7ae
Dylan Lee <dylan.lee@ertcorp.com>
2025-02-26 20:43:32 -0500

    Update hand-db.sql
    metrics table linked to hydrotable through benchmark ROI and catchment geometries instead of huc8s. This simplifies things.

--- schema/hand-db.sql
+++ schema/hand-db.sql
@@ -99,10 +99,10 @@ CREATE TABLE Hydrotables (
     PRIMARY KEY (catchment_id, hand_version_id, HydroID, stage)
 );
 
-CREATE TABLE Benchmark_HUC_Relations (
+CREATE TABLE Benchmark_Catchment_Relations (
     relation_id UUID PRIMARY KEY,
     benchmark_geom GEOMETRY,
-    huc_id TEXT REFERENCES HUCS(huc_id),
+    catchment_id UUID REFERENCES Catchments(catchment_id),
     CONSTRAINT enforce_geom_type CHECK (ST_GeometryType(benchmark_geom) IN ('ST_MultiPolygon', 'ST_Polygon'))
 );
 
@@ -124,7 +124,7 @@ CREATE TABLE HAND_Catchment_Rasters (
 CREATE TABLE Metrics (
     metric_id UUID PRIMARY KEY,
     hand_version_id TEXT REFERENCES Hand_Versions(hand_version_id),
-    relation_id UUID REFERENCES Benchmark_HUC_Relations(relation_id),
+    relation_id UUID REFERENCES Benchmark_Catchment_Relations(relation_id),
     benchmark_geom GEOMETRY,
     ver_env TEXT,
     lid TEXT,
@@ -147,5 +147,5 @@ CREATE INDEX idx_nwm_features_geometry ON NWM_Features USING GIST (geometry);
 CREATE INDEX idx_nwm_lakes_geometry ON NWM_Lakes USING GIST (geometry);
 CREATE INDEX idx_levees_geometry ON Levees USING GIST (geometry);
 CREATE INDEX idx_hucs_geometry ON HUCS USING GIST (geometry);
-CREATE INDEX idx_benchmark_huc_relations_geometry ON Benchmark_HUC_Relations USING GIST (benchmark_geom);
+CREATE INDEX idx_benchmark_catchment_relations_geometry ON Benchmark_Catchment_Relations USING GIST (benchmark_geom);
 CREATE INDEX idx_metrics_geometry ON Metrics USING GIST (benchmark_geom);
